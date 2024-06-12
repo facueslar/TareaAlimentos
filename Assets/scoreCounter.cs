71d0e9cd1e02669a34ab79c88fe372115f14effe
facueslar <facundoeslar@gmail.com>
2024-06-12 13:24:13 -0300

    Initial commit

---
+++ Assets/scoreCounter.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class scoreCounter : MonoBehaviour
+{
+    public Text txtScore;
+    public int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtScore.text = "0";
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("contacto");
+        AlimentoScript alimento;
+        alimento = collision.gameObject.GetComponent<AlimentoScript>();
+        score += alimento.valorAlimentario; 
+        txtScore.text = score.ToString();
+    }
+}
